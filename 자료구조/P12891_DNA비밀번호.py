e7021f0f4d2ba25f971db42bbea1952f3449af7c
unknown <giyun3632@naver.com>
2023-09-22 00:40:01 +0900

    DNA 비밀번호, 최솟값 찾기

---
+++ 자료구조/P12891_DNA비밀번호.py
@@ -0,0 +1,69 @@
+# 📌 문제 009) DNA 비밀번호
+# 시간 제한 2초, 실버 V, 백준 12891번
+
+checkList = [0] * 4
+myList = [0] * 4
+checkSecret = 0
+
+# 함수 정의
+def myadd(c): # 새로 들어온 문자를 처리하는 함수
+    global checkList, myList, checkSecret
+    if c == 'A':
+        myList[0] += 1
+        if myList[0] == checkList[0]:
+            checkSecret += 1
+    elif c == 'C':
+        myList[1] += 1
+        if myList[1] == checkList[1]:
+            checkSecret += 1
+    elif c == 'G':
+        myList[2] += 1
+        if myList[2] == checkList[2]:
+            checkSecret += 1
+    elif c == 'T':
+        myList[3] += 1
+        if myList[3] == checkList[3]:
+            checkSecret += 1
+            
+def myremove(c): # 제거되는 문자를 처리하는 함수
+    global checkList, myList, checkSecret
+    if c == 'A':
+        if myList[0] == checkList[0]:
+            checkSecret -= 1
+        myList[0] -= 1
+    elif c == 'C':
+        if myList[1] == checkList[1]:
+            checkSecret -= 1
+        myList[1] -= 1
+    elif c == 'G':
+        if myList[2] == checkList[2]:
+            checkSecret -= 1
+        myList[2] -= 1
+    elif c == 'T':
+        if myList[3] == checkList[3]:
+            checkSecret -= 1
+        myList[3] -= 1
+
+S, P = map(int, input().split())
+Result = 0
+A = list(input())
+checkList = list(map(int, input().split()))
+
+for i in range(4):
+    if checkList[i] == 0:
+        checkSecret += 1
+
+for i in range(P): # 초기 P 부분 문자열 처리 부분
+    myadd(A[i])
+
+if checkSecret == 4: # 4 자릿수와 관련된 크기가 모두 충족될 때 유효한 비밀번호
+     Result += 1
+
+for i in range(P, S):
+    j = i - P
+    myadd(A[i])
+    myremove(A[j])
+    if checkSecret == 4:
+        Result += 1
+
+print(Result)
