530195f2398b1a4827286ad059fda1ff111269e1
jiyoongrace <giyun3632@naver.com>
2023-10-08 17:14:04 +0900

    특정 거리의 도시 찾기

--- 그래프/P18352_특정거리도시찾기.py
+++ 그래프/P18352_특정거리도시찾기.py
@@ -1,1 +1,39 @@
 # 📌 문제 046) 특정 거리의 도시 찾기
+# 시간 제한 2초, 실버 II, 백준 18352번
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, M, K, X = map(int, input().split()) # 노드의 수, 에지의 수, 목표 거리, 시작점
+A = [[] for _ in range(N+1)]
+answer = []
+visited = [-1] * (N+1)
+
+def BFS(v): # BFS 탐색 함수 구현
+    queue = deque()
+    queue.append(v)
+    visited[v] += 1
+    while queue:
+        now_Node = queue.popleft()
+        for i in A[now_Node]:
+            if visited[i] == -1:
+                visited[i] = visited[now_Node] + 1
+                queue.append(i)
+
+for _ in range(M):
+    S, E = map(int, input().split())
+    A[S].append(E)
+
+BFS(X)
+
+for i in range(N+1):
+    if visited[i] == K:
+        answer.append(i)
+
+if not answer:
+    print(-1)
+else:
+    answer.sort()
+    for i in answer:
+        print(i)
