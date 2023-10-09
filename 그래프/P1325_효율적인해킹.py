a04ad7d0798aedf65002f62345c0e90159aa24ad
unknown <giyun3632@naver.com>
2023-10-09 23:27:57 +0900

    효율적으로 해킹하기

---
+++ 그래프/P1325_효율적인해킹.py
@@ -0,0 +1,37 @@
+# 📌 문제 047) 효율적으로 해킹하기
+# 시간 제한 5초, 실버 I, 백준 1325번
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+N, M = map(int, input().split())
+A = [[] for _ in range(N+1)]
+answer = [0] * (N+1)
+
+def BFS(v): # BFS 탐색 함수 구현
+    queue = deque()
+    queue.append(v)
+    visited[v] = True
+    while queue:
+        now_Node = queue.popleft()
+        for i in A[now_Node]:
+            if not visited[i]:
+                visited[i] = True
+                answer[i] += 1 # 신규 노드 인덱스의 정답 리스트값을 증가
+                queue.append(i)
+
+for _ in range(M):
+    S, E = map(int, input().split())
+    A[S].append(E)
+
+for i in range(1, N+1): # 모든 노드에서 BFS 실행
+    visited = [False] * (N+1)
+    BFS(i)
+
+maxVal = 0
+for i in range(1, N+1):
+    maxVal = max(maxVal, answer[i])
+
+for i in range(1, N+1):
+    if maxVal == answer[i]:
+        print(i, end=' ')
