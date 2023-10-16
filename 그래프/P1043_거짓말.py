5a2d0221c1328e278b148a1f954c4604c821c3c4
jiyoongrace <giyun3632@naver.com>
2023-10-16 22:35:23 +0900

    여행 계획 짜기, 거짓말쟁이가 되긴 싫어, 줄 세우기

--- 그래프/P1043_거짓말.py
+++ 그래프/P1043_거짓말.py
@@ -1,2 +1,54 @@
 # 📌 문제 052) 거짓말쟁이가 되긴 싫어
 # 시간 제한 2초, 골드 IV, 백준 1976번
+
+N, M = map(int, input().split())
+trueP = list(map(int, input().split()))  # 진실을 아는 사람 저장
+T = trueP[0]
+del trueP[0]
+result = 0
+party = [[] for _ in range(M)]
+
+def find(a):
+    if a == parent[a]:
+        return a
+    else:
+        parent[a] = find(parent[a])
+        return parent[a]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a != b:
+        parent[b] = a
+
+def checkSame(a, b):
+    a = find(a)
+    b = find(b)
+    if a == b:
+        return True
+    return False
+
+for i in range(M):
+    party[i] = list(map(int, input().split()))  # 파티 데이터 저장
+    del party[i][0]
+
+parent = [0] * (N + 1)
+for i in range(N + 1):  # 대표 노드를 자기 자신으로 초기화
+    parent[i] = i
+
+for i in range(M):  # 각 파티에 참여한 사람들을 1개의 그룹으로 만들기
+    firstPeople = party[i][0]
+    for j in range(1, len(party[i])):
+        union(firstPeople, party[i][j])
+
+#  각 파티의 대표 노드와 진실을 아는 사람들의 대표 노드가 같다면 과장할 수 없음
+for i in range(M):
+    isPossible = True
+    cur = party[i][0]
+    for j in range(len(trueP)):
+        if find(cur) == find(trueP[j]):
+            isPossible = False
+            break
+    if isPossible:
+        result += 1  # 모두 다르면 결괏값 1 증가
+print(result)
---
+++ 그래프/P2252_줄세우기.py
@@ -0,0 +1,25 @@
+# 📌 문제 053) 줄 세우기
+# 시간 제한 2초, 골드 III, 백준 2252번
+
+from collections import deque
+N, M = map(int, input().split())
+A = [[] for _ in range(N + 1)]
+indegree = [0] * (N + 1)  # 진입차수 배열
+
+for i in range(M):
+    S, E = map(int, input().split())
+    A[S].append(E)
+    indegree[E] += 1  # 집입차수 데이터 저장
+
+queue = deque()
+for i in range(1, N + 1):
+    if indegree[i] == 0:
+        queue.append(i)
+
+while queue:  # 위상정렬 수행
+    now = queue.popleft()
+    print(now, end=' ')
+    for next in A[now]:
+        indegree[next] -= 1
+        if indegree[next] == 0:
+            queue.append(next)
